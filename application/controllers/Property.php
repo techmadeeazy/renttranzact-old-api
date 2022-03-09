6116ea1d66f11060eee5bb370eeda6e268a91369
Temidayo <temidayo.joe@gmail.com>
2022-03-09 12:36:27 +0100

    Add posting and updating property listing.

--- application/controllers/Property.php
+++ application/controllers/Property.php
@@ -16,25 +16,76 @@ class Property extends REST_Controller
         //$this->load->model('Base_model');
     }
 
-    public function index_get(){
+    public function index_get()
+    {
         $this->response(['Welcome']);
     }
 
     /**
      * Get all property listing
      */
-    public function listing_get(){
+    public function listing_get()
+    {
         $data = [];
         $this->load->model('Property_model');
 
         $data = $this->Property_model->getAvailableActivePublic();
 
-        $this->response(['status'=>'success','data' => $data]);
+        $this->response(['status' => 'success', 'data' => $data]);
     }
-/**
- * Upload a property listing
- */
-    public function listing_post(){
+    /**
+     * Upload a property listing
+     */
+    public function listing_post()
+    {
+        $userAuthId = $this->post('user_auth_id');
+        $loginToken = $this->post('token');
+        $this->load->model('UserAuth_model');
+        $userData = $this->UserAuth_model->getById($userAuthId);
+        if (isset($userData['token']) && $userData['token'] === $loginToken) {
+            $this->load->model('Property_model');
+            $data['user_auth_id'] = $userAuthId;
+            $data['property_code'] = $this->post('property_code');
+            $data['description'] = $this->post('description');
+            $data['address'] = $this->post('address');
+            $data['state'] = $this->post('state');
 
+            $id = $this->Property_model->insertData($data);
+            $this->response(['status' => 'success','data' => ['id' => $id]]);
+        }
+        else{
+            $this->response(['status' => 'fail','message' => 'Please login']);
+        }
+    }
+    public function update_listing_post()
+    {
+        $userAuthId = $this->post('user_auth_id');
+        $loginToken = $this->post('token');
+        $this->load->model('UserAuth_model');
+        $userData = $this->UserAuth_model->getById($userAuthId);
+        if (isset($userData['token']) && $userData['token'] === $loginToken) {
+            $this->load->model('Property_model');
+            $propertyId = $this->post('id');
+            
+
+            $propertyData = $this->Property_model->getById($propertyId);
+            if ($propertyData['user_auth_id'] != $userAuthId){
+                $this->response(['status' => 'fail','message' => 'Please login']);    
+            }
+
+            $data['user_auth_id'] = $userAuthId;
+            $data['property_code'] = $this->post('property_code');
+            $data['description'] = $this->post('description');
+            $data['address'] = $this->post('address');
+            $data['state'] = $this->post('state');
+
+
+            $this->Property_model->updateById($data, $propertyId);
+    
+            $this->response(['status' => 'success','data' => ['id' => $propertyId]]);
+        }
+        else{
+            $this->response(['status' => 'fail','message' => 'Please login']);
+        }
     }
 }
--- application/models/Property_model.php
+++ application/models/Property_model.php
@@ -1,58 +1,41 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Property_model extends CI_Model {
+class Property_model extends CI_Model
+{
     private $table = 'property_listings';
 
-    public function __construct() {
+    public function __construct()
+    {
 
         parent::__construct();
         $this->load->database();
     }
 
-    public function insertData($data) {
-        $exist = [];
-       if (isset($data['id'])) {
-            //$exist = $this->getUserById($data['id']);
-        }
-        if (empty($exist)) {
-            $this->db->insert('users', $data);
-        }
-    }
-    public function getAvailableActivePublic() {
-        $query = $this->db->query("SELECT * FROM $this->table WHERE 1");
-        return $query->result_array();
-    }
-    
-    
-
-    public function updatePaymentData($reference, $data) {
+    public function insertData($data)
+    {
         $data['modified'] = date("Y-m-d H:i:s");
-        $this->db->where('reference', $reference);
-        $this->db->update('payments', $data);
-    }
-    public function updatePaymentDataWhere($where, $data) {
-        $data['modified'] = date("Y-m-d H:i:s");
-        $this->db->where($where);
-        $this->db->update('payments', $data);
+        $data['created'] = date("Y-m-d H:i:s");
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
     }
 
-    public function getPaymentByReference($reference) {
-        $query = $this->db->query("SELECT * FROM payments WHERE reference = '$reference'");
+    public function getById($id)
+    {
+        $query = $this->db->query("SELECT * FROM $this->table WHERE id = '$id'");
         return $query->row_array();
     }
 
-    /**
-     * Get all rows in the payments table
-     * @return array
-     */
-    public function getPayments() {
-        $query = $this->db->query("SELECT * FROM payments WHERE 1");
-        return $query->result_array();
+    public function updateById($data, $id)
+    {
+        $data['modified'] = date("Y-m-d H:i:s");
+        $this->db->where('id', $id);
+        $this->db->update($this->table, $data);
     }
 
-    public function rawLog($title, $body) {
+    public function rawLog($title, $body)
+    {
 
         if (is_array($body)) {
             $body = json_encode($body);
@@ -71,5 +54,4 @@ class Property_model extends CI_Model {
             'created' => date('Y-m-d H:i:s'),
         ));
     }
-
 }
