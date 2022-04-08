74c0baae48ac08ad97bbe633e1c8222c356fbc08
Temidayo <temidayo.joe@gmail.com>
2022-04-09 00:19:39 +0100

    Add REMITA function to generate RRR

--- application/config/app_live.php
+++ application/config/app_live.php
@@ -4,3 +4,4 @@ $config['app_mode'] = 'LIVE';
 $config['remita_merchant_id']  = "2547916";
 $config['remita_api_key'] = "1946";
 $config['remita_service_type_id'] = "4430731"; 
+$config['remita_pay_url'] = 'https://remitademo.net/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit';
--- application/controllers/Payment.php
+++ application/controllers/Payment.php
@@ -53,8 +53,8 @@ class Payment extends REST_Controller
             $reference = random_string('md5');
             $totalAmount = floatval($bookingData['agreed_amount']) + floatval($bookingData['caution_fee']);
             $this->load->model('Payment_model');
-            $processorReference = $this->getRemitaRRR($reference,$totalAmount); 
-            $paymentData = ['reference' => $reference,'processor_reference' => $processorReference,'inspection_booking_id' => $bookingData['id'],'amount' => $totalAmount,'user_id' => $userAuthId,  ];
+            $processorReference = $this->getRemitaRRR($reference, $totalAmount);
+            $paymentData = ['reference' => $reference, 'processor_reference' => $processorReference, 'inspection_booking_id' => $bookingData['id'], 'amount' => $totalAmount, 'user_id' => $userAuthId,];
             $this->Payment_model->insertData($paymentData);
 
             $this->response(['status' => 'success', 'message' => 'Payment started', 'data' => $paymentData]);
@@ -62,27 +62,17 @@ class Payment extends REST_Controller
         $this->response(['status' => 'fail', 'message' => 'Please login']);
     }
 
-    private function getRemitaRRR($orderId,$totalAmount,$otherData='')
+    private function getRemitaRRR($orderId, $totalAmount, $otherData = '')
     {
-        $this->load->helper('string');
-        return random_string('basic');
-    }
-
-    public function remita_rrr2_get()
-    {
-        $this->load->helper('string');
-       // echo random_string('basic');
         $this->load->config('app');
         $orderId = md5(time());
-        $totalAmount = 100;
         $apiKey = $this->config->item('remita_api_key');
         $apiHash = hash('sha512', $this->config->item('remita_merchant_id') . $this->config->item('remita_service_type_id')
             . $orderId . $totalAmount . $apiKey);
 
         $curl = curl_init();
-
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://remitademo.net/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit',
+            CURLOPT_URL => $this->config->item('remita_pay_url'),
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -91,78 +81,38 @@ class Payment extends REST_Controller
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => '{
-          "serviceTypeId": "' . $this->config->item('remita_service_type_id') . '",
-          "amount": ' . $totalAmount . ',
-          "orderId": "' . $orderId . '",
-          "payerName": "Joe Olu",
-          "payerEmail": "temidayo.joe@gmail.com",
-          "payerPhone": "08034760836",
-          "description": "Test payment"
-      }',
+              "serviceTypeId": "' . $this->config->item('remita_service_type_id') . '",
+              "amount": ' . $totalAmount . ',
+              "orderId": "' . $orderId . '",
+              "payerName": "Joe Olu",
+              "payerEmail": "temidayo.joe@gmail.com",
+              "payerPhone": "08034760836",
+              "description": "Test payment"
+          }',
             CURLOPT_HTTPHEADER => array(
                 'Authorization: remitaConsumerKey=' . $this->config->item('remita_merchant_id') . ',remitaConsumerToken=' . $apiHash . '',
                 'Content-Type: application/json'
             ),
         ));
-
-        $response = curl_exec($curl);
-
+        $jsopResponse = curl_exec($curl);
         curl_close($curl);
-        echo $response;
-        $this->response(['status' => 'success','data' => ['api_key' => $apiKey,'orderId' => $orderId, 'amount' => $totalAmount,'api_hash' => $apiHash ],
-        'service_type_id' => $this->config->item('remita_service_type_id'),
-        'merchant_id' => $this->config->item('remita_merchant_id') , 'response' => json_decode($response,false)]);
+        $response = json_decode(trim($jsopResponse, "jsonp ( )"), true);
+        if (isset($response['statuscode']) && $response['statuscode'] == '025') {
+            return $response['RRR'];
+        }
+        return null;
     }
 
     public function remita_rrr_get()
     {
-        $curl = curl_init();
+        $this->load->helper('string');
+        // echo random_string('basic');
+        $this->load->config('app');
+        $orderId = md5(time());
+        $totalAmount = 100;
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://remitademo.net/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{
-          "serviceTypeId": "4430731",
-          "amount": "100",
-          "orderId": "' . md5(time()) . '",
-          "payerName": "Joe Olu",
-          "payerEmail": "temidayo.joe@gmail.com",
-          "payerPhone": "08034760836",
-          "description": "Test payment",
-          "lineItems": [
-              {
-                  "lineItemsId": "username1",
-                  "beneficiaryName": "Val Henry",
-                  "beneficiaryAccount": "6020067886",
-                  "bankCode": "058",
-                  "beneficiaryAmount": "70",
-                  "deductFeeFrom": "0"
-              },
-              {
-                  "lineItemsId": "itemid2",
-                  "beneficiaryName": "Sam Ola",
-                  "beneficiaryAccount": "0360883515",
-                  "bankCode": "058",
-                  "beneficiaryAmount": "20",
-                  "deductFeeFrom": "0"
-              }
-          ]
-      }',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: remitaConsumerKey=2547916,remitaConsumerToken=a3dd2e50d69f6e928d02d5a995aa059c80257edb530282f8ba27a2df6e39e8182e77aedb66f4e55771d5c6b67f426364e010e2dc970bfbef8b8e449164c67621',
-                'Content-Type: application/json'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        echo $response;
+        $this->response([
+            'status' => 'success', 'data' => ['RRR' => $this->getRemitaRRR($orderId, $totalAmount)]
+        ]);
     }
 }
