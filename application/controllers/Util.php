62a9f896fe55c5a7d5e4cb5e3ede69fe913eecb2
Temidayo <temidayo.joe@gmail.com>
2022-04-12 17:02:15 +0100

    Get Banks endpoint

---
+++ application/controllers/Util.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * 
+ *
+ * @author Temidayo Oluwabusola
+ */
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Util extends REST_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //$this->load->model('Base_model');
+    }
+
+
+    public function bank_get($bankgId='')
+    {
+        $this->load->model('Bank_model');
+        $bankData = $this->Bank_model->getAll();
+        $this->response(['status' => 'success', 'data' => $bankData]);
+    }
+
+
+}
