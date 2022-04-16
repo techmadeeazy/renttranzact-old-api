4b8926c12c7a6e5526e6310c4eaff9bee37759f1
Temidayo <temidayo.joe@gmail.com>
2022-04-16 08:23:52 +0100

    Add bank account enquiry

--- application/controllers/Util.php
+++ application/controllers/Util.php
@@ -33,7 +33,7 @@ class Util extends REST_Controller
         $accountNumber = $this->post('account_number');
         $bankCode = $this->post('bank_code');
 
-
+        $remitaAccessToken = $this->getRemitaAccessToken();
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
@@ -52,13 +52,11 @@ class Util extends REST_Controller
 ',
             CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/json',
-                'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJtUC05LVFXRmczVWMxTjNQZjE0SnFYZldSc2lCUl9DcnA5LWtjVjE0cFdjIn0.eyJleHAiOjE2NTAwMTIxNjUsImlhdCI6MTY1MDAwODU2NSwianRpIjoiMDhhNTM0MDItNmJhMC00NDEwLTg5NzQtM2QxZjdiMWQ4MDViIiwiaXNzIjoiaHR0cDovLzEwLjEuMS44Mjo5MTgwL2tleWNsb2FrL3JlbWl0YS9leGFwcC9hcGkvdjEvcmVkZ2F0ZS9hdXRoL3JlYWxtcy9yZW1pdGEiLCJhdWQiOlsiZGlzY292ZXJ5LXNlcnZlciIsImFjY291bnQiXSwic3ViIjoiZjU0ODNmMTYtNTQzYS00ZjI1LWJjMTYtNGNkMzZjMjBlNTlhIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoicmVtaXRhdWFhLXNlcnZpY2UiLCJzZXNzaW9uX3N0YXRlIjoiODgyOTY4ODUtNzRiYS00Y2YwLWJkNjYtZTk3MjQ4ZDI0N2M3IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2xvZ2luLnJlbWl0YS5uZXQiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJkaXNjb3Zlcnktc2VydmVyIjp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50Iiwidmlldy1wcm9maWxlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6IkFERUJBWU8gQURFQkFZTyIsInByZWZlcnJlZF91c2VybmFtZSI6InVoc3U2emltYXZ4bnpoeHciLCJnaXZlbl9uYW1lIjoiQURFQkFZTyIsImZhbWlseV9uYW1lIjoiQURFQkFZTyIsIm9yZ2FuaXNhdGlvbi1pZCI6IkNXR0RFTU8iLCJlbWFpbCI6IjAxMiJ9.ACvhRfPFoFIk4vbJuYaC3WUCfXY_yogb4ChvFly4S6LLoeHLDRiKB0TXFovVdVCJKotvb_A4PBrW5naAnkPnhCGPtvi4osrZQWu6N_KARlsr9pE_MSf9z3qL-DfEGFcyVdxn6h8JUu6Tnrj9B75WhYiZeB0hpWleHpbHt-ysY_0ND0VVuWBD8NDlPvmaiLEkgZicPd3P5HfSez5qiPKJiYXqAutFLgAVHo8xgOeZDI-8m8_fPLNb3I6KafksgdxJw7ufnw0yhgBgSvmvDxY-WnAKZSaKzr7MK_yLhiCKtO0Bbq3mrir9ReY6D-EUfZAxY88PXngwfFN6nFSmoWmy1g',
-                'Cookie: JSESSIONID=AVKyxaXKtV3_DXcda1qzxwsE.9eb07e7cd645'
+                'Authorization: Bearer '. $remitaAccessToken
             ),
         ));
 
         $response = curl_exec($curl);
-
         curl_close($curl);
         echo $response;
     }
