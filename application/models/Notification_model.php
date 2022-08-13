1608b3d2dc41b90c52ab552aefff8efa8b2b8508
Temidayo <temidayo.joe@gmail.com>
2022-08-13 21:25:20 +0100

    Fix email notification to property owner

--- application/models/Notification_model.php
+++ application/models/Notification_model.php
@@ -64,7 +64,8 @@ class Notification_model extends CI_Model
 
   public function sendBookingEmail($bookingData)
   {
-    $hostData = $this->getUserById($bookingData['user_auth_id']);
+    $this->load->model('User_model');
+    $hostData = $this->User_model->getUserById($bookingData['user_auth_id']);
     $mailBody = "Your property [" . $bookingData['title'] . "] has just being booked for inspection.
     <br>Please check your app and respond to the request within 72hours.
     <br>For any enquiries contact  Support Team at customersupport@renttranzact.com"
