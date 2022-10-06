b2cca59fbc23b6c597601f53a12655be767235b4
Temidayo <temidayo.joe@gmail.com>
2022-10-06 12:06:43 +0100

    Add duration to property listing and update

--- application/controllers/Property.php
+++ application/controllers/Property.php
@@ -93,7 +93,7 @@ class Property extends REST_Controller
             $data['purpose'] = $this->post('purpose');
             $data['management_fee_percent'] = $this->post('management_fee_percent');
             $data['management_fee_percent'] = intval($data['management_fee_percent']) < 5 ? 10 : $data['management_fee_percent'];
-            
+            $data['duration'] = $this->post('duration');
 
 
             $id = $this->Property_model->insertData($data);
@@ -133,7 +133,8 @@ class Property extends REST_Controller
             $data['display'] = strtolower($this->post('display')) == 'public' ? 'public' : 'private';
             $data['management_fee_percent'] = $this->post('management_fee_percent');
             $data['management_fee_percent'] = intval($data['management_fee_percent']) < 5 ? 10 : $data['management_fee_percent'];
-
+            $data['duration'] = $this->post('duration');
+            
             $this->Property_model->updateById($data, $propertyId);
             $this->response(['status' => 'success', 'data' => ['id' => $propertyId]]);
         } else {
