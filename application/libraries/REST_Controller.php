ab3588352be457ccffacf93ad22185c07641674a
Temidayo <temidayo.joe@gmail.com>
2022-11-07 14:45:20 +0100

    Fix forward compability with PHP8 in the REST library

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -1009,7 +1009,7 @@ abstract class REST_Controller extends CI_Controller {
                 $method = $this->input->server('HTTP_X_HTTP_METHOD_OVERRIDE');
             }
 
-            $method = strtolower($method);
+            $method = empty($method) ? $method : strtolower($method);
         }
 
         if (empty($method))
